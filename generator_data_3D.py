1be34e42c15b472e181b43fdeff675a8a030a627
cedric <allierc@janelia.hhmi.org>
2023-11-28 15:19:45 -0500

    start training config_list = ['config_recons_from_beads']

--- generator_data_3D.py
+++ generator_data_3D.py
@@ -24,6 +24,37 @@ import yaml # need to install pyyaml
 import datetime
 from torch.fft import fft2, ifft2, fftshift, ifftshift
 
+def data_generate():
+    print(' ')
+
+    y_batches = torch.zeros((bpm.n_gammas + 1, 30, image_width, image_width), device=device)
+
+    start = timer()
+    phiL = torch.rand([bpm.image_width, bpm.image_width, 1000], dtype=torch.float32, requires_grad=False,
+                      device='cuda:0') * 2 * np.pi
+
+    for plane in tqdm(range(0, bpm.Nz)):
+        I = torch.tensor(np.zeros((bpm.n_gammas + 1, bpm.image_width, bpm.image_width)), device=bpm.device,
+                         dtype=bpm.dtype, requires_grad=False)
+
+        for w in range(0, Niter):
+            zoi = np.random.randint(1000 - bpm.Nz)
+            with torch.no_grad():
+                I = I + bpm(plane=int(plane), phi=phiL[:, :, zoi:zoi + bpm.Nz], naber=0)
+        y_batches[:, plane:plane + 1, :, :] = I[:, None, :, :]
+
+    end = timer()
+    print(f'elapsed time : {np.round(end - start, 2)}')
+
+    for k in range(bpm.n_gammas + 1):
+        imwrite(f'./{log_dir}/stack_{k}.tif', y_batches[k].detach().cpu().numpy().squeeze())
+
+    torch.save(y_batches, f'./{log_dir}/y_batches.pt')
+
+def data_train():
+    y_batches = torch.load(model_config['input_fluo_simulated_acquisition'], map_location=device)
+
+
 if __name__ == '__main__':
 
     print('Init ...')
@@ -41,143 +72,64 @@ if __name__ == '__main__':
     print(f'num_polynomials: {num_polynomials}')
     print(f'Niter: {Niter}')
 
-    config = 'config_grid'
+    config_list = ['config_recons_from_beads'] #['config_grid', 'config_beads', 'config_beads_cropped','config_boats']
 
-    # Create log directory
-    l_dir = os.path.join('.', 'log', config)
-    log_dir = os.path.join(l_dir, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-    print('log_dir: {}'.format(log_dir))
-    os.makedirs(log_dir, exist_ok=True)
-    # os.makedirs(os.path.join(log_dir, 'viz'), exist_ok=True)
+    for config in config_list:
 
-    with open(f'./config/{config}.yaml', 'r') as file:
-        model_config = yaml.safe_load(file)
+        print (f'run :{config}')
 
-    torch.cuda.empty_cache()
-    bpm = bpmPytorch(model_config)      # just to get the pupil function
+        # Create log directory
+        l_dir = os.path.join('.', 'log', config)
+        log_dir = os.path.join(l_dir, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        print('log_dir: {}'.format(log_dir))
+        os.makedirs(log_dir, exist_ok=True)
+        copyfile(os.path.realpath(__file__), os.path.join(log_dir, 'generating_code.py'))
 
-    num_polynomials = model_config['num_polynomials_gammas'] - 3  # NOTE: don't include piston and tip/tilt
+        with open(f'./config/{config}.yaml', 'r') as file:
+            model_config = yaml.safe_load(file)
 
-    conversion_factor_rms_pi = 2 * np.pi / bpm.wavelength  # Conversion factor to convert RMS between phase and length units
-    ansi_indices = np.arange(3, num_polynomials + 3)  # NOTE: don't include piston and tip/tilt
-    zernike_coefficients_gt = model_config['zernike_coefficients_gt']
-    zernike_coefficients_gt = [z * conversion_factor_rms_pi for z in zernike_coefficients_gt]
+        torch.cuda.empty_cache()
+        bpm = bpmPytorch(model_config)      # just to get the pupil function
 
-    input_gammas_zernike = conversion_factor_rms_pi * np.array(model_config['input_gammas_zernike'])
-    n_gammas = model_config['n_gammas']
-    p_num, p_unit = model_config['pixel_size'].split()
-    pixel_size = float(p_num) * u.Unit(p_unit)
-    numerical_aperture = model_config['numerical_aperture']
-    w_num, w_unit = model_config['wavelength'].split()
-    wavelength = float(w_num) * u.Unit(w_unit)
-    image_width=bpm.image_width
+        num_polynomials = model_config['num_polynomials_gammas'] - 3  # NOTE: don't include piston and tip/tilt
 
-    pupil = Pupil(numerical_aperture=numerical_aperture, wavelength=wavelength, pixel_size=pixel_size,
-                  size_fourier_space=(image_width, image_width), device=DEVICE)
-    zernike_instance = ZernikePolynomials(pupil, index_convention='ansi', normalization=False)
-    phase_aberration_gt = zernike_instance.get_aberration(ansi_indices, zernike_coefficients_gt)
-    phase_aberration_gt = torch.FloatTensor(phase_aberration_gt).to(DEVICE)
-    # phase_aberration_gt = phase_aberration_gt.repeat(n_gammas + 1, 1, 1)
+        conversion_factor_rms_pi = 2 * np.pi / bpm.wavelength  # Conversion factor to convert RMS between phase and length units
+        ansi_indices = np.arange(3, num_polynomials + 3)  # NOTE: don't include piston and tip/tilt
+        zernike_coefficients_gt = model_config['zernike_coefficients_gt']
+        zernike_coefficients_gt = [z * conversion_factor_rms_pi for z in zernike_coefficients_gt]
 
-    gammas = torch.zeros(n_gammas + 1, 1, image_width, image_width, device=DEVICE, dtype=torch.complex128)
-    for i in range(n_gammas + 1):
-       phase_shift = fftshift ( torch.tensor(zernike_instance.get_aberration(ansi_indices, input_gammas_zernike[i]),device=DEVICE) * pupil.values + phase_aberration_gt)
-       # phase_shift = fftshift(torch.tensor(zernike_instance.get_aberration(ansi_indices, input_gammas_zernike[i]),device=DEVICE) * pupil.values)
-       gammas[i, :, :, :] = torch.exp(1j * phase_shift)
-    gammas = gammas.squeeze()
-    bpm.gammas = gammas
+        input_gammas_zernike = conversion_factor_rms_pi * np.array(model_config['input_gammas_zernike'])
+        n_gammas = model_config['n_gammas']
+        p_num, p_unit = model_config['pixel_size'].split()
+        pixel_size = float(p_num) * u.Unit(p_unit)
+        numerical_aperture = model_config['numerical_aperture']
+        w_num, w_unit = model_config['wavelength'].split()
+        wavelength = float(w_num) * u.Unit(w_unit)
+        image_width=bpm.image_width
 
-    print(' ')
+        pupil = Pupil(numerical_aperture=numerical_aperture, wavelength=wavelength, pixel_size=pixel_size,
+                      size_fourier_space=(image_width, image_width), device=DEVICE)
+        zernike_instance = ZernikePolynomials(pupil, index_convention='ansi', normalization=False)
+        phase_aberration_gt = zernike_instance.get_aberration(ansi_indices, zernike_coefficients_gt)
+        phase_aberration_gt = torch.FloatTensor(phase_aberration_gt).to(DEVICE)
+        # phase_aberration_gt = phase_aberration_gt.repeat(n_gammas + 1, 1, 1)
 
-    y_batches = torch.zeros((bpm.n_gammas+1, 30, image_width, image_width), device=device)
+        gammas = torch.zeros(n_gammas + 1, 1, image_width, image_width, device=DEVICE, dtype=torch.complex128)
+        for i in range(n_gammas + 1):
+           # phase_shift = fftshift ( torch.tensor(zernike_instance.get_aberration(ansi_indices, input_gammas_zernike[i]),device=DEVICE) * pupil.values + phase_aberration_gt)
+           phase_shift = fftshift(torch.tensor(zernike_instance.get_aberration(ansi_indices, input_gammas_zernike[i]),device=DEVICE) * pupil.values)
+           gammas[i, :, :, :] = torch.exp(1j * phase_shift)
+        gammas = gammas.squeeze()
+        bpm.gammas = gammas
 
-    start = timer()
-    phiL = torch.rand([bpm.image_width, bpm.image_width, 1000], dtype=torch.float32, requires_grad=False, device='cuda:0') * 2 * np.pi
 
-    for plane in tqdm(range(0, bpm.Nz)):
-        I = torch.tensor(np.zeros((bpm.n_gammas+1,bpm.image_width, bpm.image_width)), device=bpm.device, dtype=bpm.dtype, requires_grad=False)
+        if 'y_batches.pt' in model_config['input_fluo_simulated_acquisition']:
 
-        for w in range(0, Niter):
-            zoi = np.random.randint(1000 - bpm.Nz)
-            with torch.no_grad():
-                I = I + bpm(plane=int(plane), phi=phiL[:, :, zoi:zoi + bpm.Nz], naber=0)
-        y_batches[:, plane:plane + 1, :, :] = I[:,None,:,:]
+            data_train()
 
-    end = timer()
-    print(f'elapsed time : {np.round(end - start,2)}')
+        else:
 
-    for k in range(bpm.n_gammas+1):
-        imwrite(f'./{log_dir}/stack_{k}.tif',y_batches[k].detach().cpu().numpy().squeeze() )
+            data_generate ()
 
 
 
-    #
-    #
-    # else :
-    #
-    #     for plane in range(0, bpm.Nz):
-    #
-    #         print(f'plane: {plane}')
-    #
-    #         z_in = torch.abs(torch.randn(100, num_polynomials, device=DEVICE))
-    #         z_in = f.normalize(z_in, p=1, dim=1)
-    #         z_in = z_in.repeat(256, 256, 1, 1).permute(2, 3, 0, 1)
-    #         basis = compute_zernike_basis(num_polynomials=num_polynomials, field_res=(PSF_size, PSF_size)).unsqueeze(0).repeat(100, 1, 1, 1)
-    #         basis = basis.to(DEVICE)
-    #         out = basis * z_in
-    #         out = torch.sum(out, dim=1) * torch.pi
-    #
-    #         out_cpx = torch.zeros(100, 256, 256, dtype=torch.cfloat, device=DEVICE)
-    #         t = fftshift(bpm.pupil)
-    #
-    #         for k in range(100):
-    #             tt = torch.exp(torch.mul(torch.abs(t) * out[k, :, :], 20j))
-    #             tt = fftshift(tt)
-    #             out_cpx[k, :, :] = tt
-    #
-    #         x_batches[:, plane:plane+1, :, :] = out_cpx[:,None,:,:]
-    #
-    #         imwrite(f'./Pics_input/stack/aberration_plane{plane}.tif',
-    #                     torch.angle(out_cpx).detach().cpu().numpy())
-    #
-    #         bpm.aber = out_cpx
-    #         bpm.aber_layers = bpm.aber.unbind(dim=0)
-    #         bpm.bAber = 2
-    #
-    #         Istack = np.zeros([bpm.image_width, bpm.image_width, 100])
-    #         phiL = torch.rand([bpm.image_width, bpm.image_width, 1000], dtype=torch.float32, requires_grad=False, device='cuda:0') * 2 * np.pi
-    #
-    #         for naber in tqdm(range(100)):
-    #
-    #             I = torch.tensor(np.zeros((bpm.image_width, bpm.image_width)), device=bpm.device, dtype=bpm.dtype, requires_grad=False)
-    #             for w in range(0, Niter):
-    #                 zoi = np.random.randint(1000 - bpm.Nz)
-    #                 with torch.no_grad():
-    #                     I = I + bpm(plane=int(plane), phi=phiL[:, :, zoi:zoi + bpm.Nz ], naber=naber)
-    #             Istack[:, :, naber] = I.detach().cpu().numpy() / Niter
-    #
-    #         tmp = torch.tensor(Istack,device=device)
-    #         tmp = torch.permute(tmp, (2, 0, 1))
-    #         y_batches[:, plane:plane + 1, :, :] = tmp[:, None, :, :]
-    #
-    #         tmp = np.moveaxis(Istack, -1, 0)
-    #         imwrite(f'./Pics_input/stack/input_aberration_plane{plane}.tif', tmp )
-    #
-    #
-    # if bSingle:
-    #     torch.save(x_batches, f'./Pics_input/x_single_batches.pt')
-    #     torch.save(y_batches, f'./Pics_input/y_single_batches.pt')
-    # else:
-    #     if bDefocus:
-    #         torch.save(x_batches,f'./Pics_input/defocus_x_batches.pt')
-    #         torch.save(y_batches, f'./Pics_input/defocus_y_batches.pt')
-    #     else:
-    #         torch.save(x_batches,f'./Pics_input/x_batches.pt')
-    #         torch.save(y_batches, f'./Pics_input/y_batches.pt')
-    #
-    #
-    #
-    #
-    #
-    #
-    #
--- networks_3D.py
+++ networks_3D.py
@@ -22,25 +22,210 @@ class sine_act(nn.Module):
         out = torch.sin(x)
         return out
 
+# class G_Renderer(nn.Module):
+#     def __init__(self, in_dim=32, hidden_dim=32, num_layers=2, out_dim=1):
+#         super().__init__()
+#         act_fn = nn.ReLU()
+#         layers = []
+#         for _ in range(num_layers):
+#             if len(layers) == 0:
+#                 layers.append(nn.Linear(in_dim, hidden_dim))
+#             else:
+#                 layers.append(nn.Linear(hidden_dim, hidden_dim))
+#             # layers.append(nn.LayerNorm(hidden_dim))
+#             layers.append(act_fn)
+#         layers.append(nn.Linear(hidden_dim, out_dim))
+#         self.net = nn.Sequential(*layers)
+#
+#     def forward(self, x):
+#         out = self.net(x)
+#         return out
 
 class G_Renderer(nn.Module):
-    def __init__(self, in_dim=32, hidden_dim=32, num_layers=2, out_dim=1):
+    def __init__(
+        self, in_dim=32, hidden_dim=32, num_layers=2, out_dim=1, use_layernorm=False
+    ):
         super().__init__()
         act_fn = nn.ReLU()
         layers = []
-        for _ in range(num_layers):
-            if len(layers) == 0:
-                layers.append(nn.Linear(in_dim, hidden_dim))
-            else:
-                layers.append(nn.Linear(hidden_dim, hidden_dim))
-            # layers.append(nn.LayerNorm(hidden_dim))
+        layers.append(nn.Linear(in_dim, hidden_dim))
+        if use_layernorm:
+            layers.append(nn.LayerNorm(hidden_dim, elementwise_affine=False))
+        layers.append(act_fn)
+        for _ in range(num_layers - 1):
+            layers.append(nn.Linear(hidden_dim, hidden_dim))
+            if use_layernorm:
+                layers.append(nn.LayerNorm(hidden_dim, elementwise_affine=False))
             layers.append(act_fn)
+
         layers.append(nn.Linear(hidden_dim, out_dim))
         self.net = nn.Sequential(*layers)
 
     def forward(self, x):
         out = self.net(x)
         return out
+
+
+class G_Tensor3D(nn.Module):
+    def __init__(
+        self, x_mode, y_mode, z_mode, z_min, z_max, num_feats=32, use_layernorm=False
+    ):
+        super().__init__()
+        self.x_mode, self.y_mode, self.num_feats = x_mode, y_mode, num_feats
+        self.data = nn.Parameter(
+            2e-4 * torch.randn((self.x_mode, self.y_mode, self.num_feats)),
+            requires_grad=True,
+        )
+        self.renderer = G_Renderer(in_dim=self.num_feats, use_layernorm=use_layernorm)
+        self.x0 = None
+
+        self.z_mode = z_mode
+        self.z_data = nn.Parameter(
+            torch.randn((self.z_mode, self.num_feats)), requires_grad=True
+        )
+        self.z_min = z_min
+        self.z_max = z_max
+        self.z_mode = z_mode
+
+    def create_coords(self, x_dim, y_dim, x_max, y_max):
+        half_dx, half_dy = 0.5 / x_dim, 0.5 / y_dim
+        xs = torch.linspace(half_dx, 1 - half_dx, x_dim)
+        ys = torch.linspace(half_dx, 1 - half_dy, y_dim)
+        xv, yv = torch.meshgrid([xs, ys], indexing="ij")
+        xy = torch.stack((yv.flatten(), xv.flatten())).t()
+        xs = xy * torch.tensor([x_max, y_max], device=xs.device).float()
+        indices = xs.long()
+        self.x_dim, self.y_dim = x_dim, y_dim
+        self.xy_coords = nn.Parameter(
+            xy[None],
+            requires_grad=False,
+        )
+
+        if self.x0 is not None:
+            device = self.x0.device
+            self.x0.data = (indices[:, 0].clamp(min=0, max=x_max - 1)).to(device)
+            self.y0.data = indices[:, 1].clamp(min=0, max=y_max - 1).to(device)
+            self.x1.data = (self.x0 + 1).clamp(max=x_max - 1).to(device)
+            self.y1.data = (self.y0 + 1).clamp(max=y_max - 1).to(device)
+            self.lerp_weights.data = (xs - indices.float()).to(device)
+        else:
+            self.x0 = nn.Parameter(
+                indices[:, 0].clamp(min=0, max=x_max - 1),
+                requires_grad=False,
+            )
+            self.y0 = nn.Parameter(
+                indices[:, 1].clamp(min=0, max=y_max - 1),
+                requires_grad=False,
+            )
+            self.x1 = nn.Parameter(
+                (self.x0 + 1).clamp(max=x_max - 1), requires_grad=False
+            )
+            self.y1 = nn.Parameter(
+                (self.y0 + 1).clamp(max=y_max - 1), requires_grad=False
+            )
+            self.lerp_weights = nn.Parameter(xs - indices.float(), requires_grad=False)
+
+    def normalize_z(self, z):
+        return (self.z_mode - 1) * (z - self.z_min) / (self.z_max - self.z_min)
+
+    def sample(self, z):
+        z = self.normalize_z(z)
+        z0 = z.long().clamp(min=0, max=self.z_mode - 1)
+        z1 = (z0 + 1).clamp(max=self.z_mode - 1)
+        zlerp_weights = (z - z.long().float())[:, None]
+
+        xy_feat = (
+            self.data[self.y0, self.x0]
+            * (1.0 - self.lerp_weights[:, 0:1])
+            * (1.0 - self.lerp_weights[:, 1:2])
+            + self.data[self.y0, self.x1]
+            * self.lerp_weights[:, 0:1]
+            * (1.0 - self.lerp_weights[:, 1:2])
+            + self.data[self.y1, self.x0]
+            * (1.0 - self.lerp_weights[:, 0:1])
+            * self.lerp_weights[:, 1:2]
+            + self.data[self.y1, self.x1]
+            * self.lerp_weights[:, 0:1]
+            * self.lerp_weights[:, 1:2]
+        )
+        z_feat = (
+            self.z_data[z0] * (1.0 - zlerp_weights) + self.z_data[z1] * zlerp_weights
+        )
+        z_feat = z_feat[:, None].repeat(1, xy_feat.shape[0], 1)
+        feat = xy_feat[None].repeat(z.shape[0], 1, 1) * z_feat
+
+        return feat
+
+    def forward(self, z):
+        feat = self.sample(z)
+
+        out = self.renderer(feat)
+        b = z.shape[0]
+        w, h = self.x_dim, self.y_dim
+        out = out.view(b, 1, w, h)
+        return out
+
+class G_Model3D(nn.Module):
+    def __init__(
+        self,
+        w,
+        h,
+        num_feats,
+        x_mode,
+        y_mode,
+        z_mode,
+        z_min,
+        z_max,
+    ):
+        """
+        Args:
+            w, h (int): image resolution
+            num_feats (int): number of features (e.g. 32)
+            x_mode, y_mode (int): feature grid resolution across x-y (e.g. 512)
+            z_mode (int): feature grid resolution along z (e.g. 64)
+            z_min (float): mininum value of z, used in function normalize_z
+            z_max (float): maximum value of z, used in function normalize_z
+        """
+        super().__init__()
+        self.img_1 = G_Tensor3D(
+            x_mode=x_mode,
+            y_mode=y_mode,
+            z_mode=z_mode,
+            z_min=z_min,
+            z_max=z_max,
+            num_feats=num_feats,
+        )
+        self.img_2 = G_Tensor3D(
+            x_mode=x_mode,
+            y_mode=y_mode,
+            z_mode=z_mode,
+            z_min=z_min,
+            z_max=z_max,
+            num_feats=num_feats,
+        )
+        self.w, self.h = w, h
+        self.init_grids()
+
+    def init_grids(self):
+        self.img_1.create_coords(
+            x_dim=self.w,
+            y_dim=self.h,
+            x_max=self.img_real.x_mode,
+            y_max=self.img_real.y_mode,
+        )
+        self.img_2.create_coords(
+            x_dim=self.w,
+            y_dim=self.h,
+            x_max=self.img_imag.x_mode,
+            y_max=self.img_imag.y_mode,
+        )
+
+    def forward(self, z):
+        # G_Tensor3D takes in a z value and returns the predicted 1-channel image of shape (w, h)
+        img_1 = self.img_1(z)
+        img_2 = self.img_2(z)
+
+        return img_1, img_2
 
 
 class G_FeatureTensor(nn.Module):
@@ -265,6 +450,8 @@ class StaticNet(ZernNet):
     def get_estimates(self):
 
         object_est = self.g_im()  # Neural representation of estimated object
+
+
         object_est = object_est.squeeze().repeat(self.n_gammas + 1, 1, 1)
 
         g_in = self.basis[0]  # Zernike basis 1x15
